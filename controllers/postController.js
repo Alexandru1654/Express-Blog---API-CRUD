72d897c5a902bddf3e6beb54dce61cc98f7652ce
alexandru233847 <alexutualexino33@gmail.com>
2024-11-06 18:50:59 +0100

    Testing the routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,18 +2,25 @@ const posts = require('../db');
 
 const store = (req, res) => {
     const { title, content, slug, image, tags } = req.body;
-}
 
-const newPost = {
-    title,
-    content,
-    slug,
-    image,
-    tags
+    if (!title || !content || !slug || !image || !tags) {
+        return res.status(400).json({ error: "Tutti i campi sono richiesti" });
+    }
+
+    const newPost = {
+        title,
+        content,
+        slug,
+        image,
+        tags
+    };
+
+    posts.push(newPost);
+
+    res.json(posts);
 };
 
-posts.push(newPost);
-
-res.json(posts);
-
 module.exports = { store };
+
+
+
--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const posts = require('../controllers/postController');
+const { store } = require('../controllers/postController');
+const posts = require('../db');
 
 router.get('/', (req, res) => {
     res.send('<ul>' + posts.map(post => `<li>${post.title}</li>`).join('') + '</ul>');
@@ -11,10 +12,52 @@ router.get('/:slug', (req, res) => {
     if (post) {
         res.json(post);
     } else {
-        res.status(404).send('Post not found');}
-    });
+        res.status(404).send('Post not found');
+    }
+});
 
-    router.post('/', store);
-    module.exports = router;
+router.post('/', store);
+module.exports = router;
 
-    
+
+router.post('/', store);
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, content, slug, image, tags } = req.body;
+
+    const postIndex = posts.findIndex(post => post.id === parseInt(id));
+
+    if (postIndex === -1) {
+        return res.status(404).json({ error: 'Post not found' });
+    }
+
+    const updatedPost = {
+        ...posts[postIndex],
+        title,
+        content,
+        slug,
+        image,
+        tags
+    };
+
+    posts[postIndex] = updatedPost;
+
+    res.json(updatedPost);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const postIndex = posts.findIndex(post => post.id === parseInt(id));
+
+    if (postIndex === -1) {
+        return res.status(404).json({ error: 'Post not found' });
+    }
+
+    posts.splice(postIndex, 1);
+
+    res.json(posts);
+});
+
+module.exports = router;
